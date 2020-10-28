4212e744abe62c6cd6848134d943fa41ebf8d0ee
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-10-29 00:22:16 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
 
-package com.company;
-
 import java.util.Scanner;
 
 public class Main {
@@ -10,10 +8,14 @@ public class Main {
         double x = readVariable("x");
         double n = readVariable("n");
         double e = readVariable("e");
+        double res = calculateRes(x);
+        double result[] = calculateSum(x, (int) n, e);
+
         if(!verifyVariableX(x)) {
             return;
         }
-        calc(x, (int)n, e);
+
+        printResult(x, n, e, res);
     }
 
     private static double readVariable(String string) {
@@ -30,36 +32,39 @@ public class Main {
         return true;
     }
 
-    private static void printResult(double sum1, double sum2, int n2, double sum3, int n3, double res) {
-        System.out.printf("1) %.3f\n", sum1);
-        System.out.printf("2) %.3f - %d слагаемых\n", sum2, n2);
-        System.out.printf("3) %.3f - %d слагаемых\n", sum3, n3);
-        System.out.printf("4) %.3f\n", res);
+    private static void printResult(double x, double n, double e, double res) {
+        double result[] = calculateSum(x, (int) n, e);
+        System.out.printf("1) Значение функции равно %.5f", result[0]);
+        System.out.println();
+        System.out.printf("2) Значение функции при %.0f слагаемых равно %.5f", result[2], result[1]);
+        System.out.println();
+        System.out.printf("3) Значение функции при %.0f слагаемых равно %.5f", result[4], result[3]);
+        System.out.println();
+        System.out.printf("4) Значение функции 1/(1+x)^2 равно %.5f", res);
     }
 
-    public static void calc(double x, int n, double e) {
-        double a = 1;
-        double sum1 = 0, sum2 = 0, sum3 = 0;
+    private static double[] calculateSum(double x, int n, double e) {
+        double R = 1;
+        double sum_n = 0, sum_e = 0, sum_e_10 = 0;
         int n2 = 0, n3 = 0;
-        for(int i = 1; i <= n || Math.abs(a) > e / 10; i++) {
+        for(int i = 1; i <= n || Math.abs(R) > e / 10; i++) {
             if(i <= n) {
-                sum1 += a;
+                sum_n += R;
             }
-            if(Math.abs(sum2 - (sum2 + a)) > e) {
-                sum2 += a;
+            if(Math.abs(sum_e - (sum_e + R)) > e) {
+                sum_e += R;
                 n2++;
             }
-            if(Math.abs(sum3 - (sum3 + a)) > e / 10) {
-                sum3 += a;
+            if(Math.abs(sum_e_10 - (sum_e_10 + R)) > e / 10) {
+                sum_e_10 += R;
                 n3++;
             }
-            a = - a * x * (i + 1) / i;
+            R = - R * x * (i + 1) / i;
         }
-        double res = calcRes(x);
-        printResult(sum1, sum2, n2, sum3, n3, res);
+        return new double[] {sum_n, sum_e, n2, sum_e_10, n3};
     }
 
-    private static double calcRes(double x) {
+    private static double calculateRes(double x) {
         return 1 / Math.pow(1 + x, 2);
     }
 }
