bfbbde01b3aa1a382bc231e254138dc76e75e0a0
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-11-04 13:21:07 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double x = readVariable("x");
-        double n = readVariable("n");
-        double e = readVariable("e");
-        double function = calculateFunction(x);
-        double result[] = calculateSum(x, (int)n, e);
+        double x = readVariable(" значение (x)");
+        double n = readVariable("колличество слагаемых (n)");
+        double e = readVariable("эпсилон (e)");
+
         if(!verifyVariableX(x)) {
             return;
         }
-        printResult(result, function);
+
+        double function = calculateFunction(x);
+        double sumOfNSlag = calculateSumOfNSlag(x, (int)n);
+        Result resultEpsilon = calculateEpsilonSum(x, e);
+        Result resultEpsilonSumOn10 = calculateEpsilonSumOn10(x, e);
+
+        printResult(sumOfNSlag, resultEpsilon, resultEpsilonSumOn10, function);
     }
 
     private static double readVariable(String string) {
@@ -24,44 +29,64 @@ public class Main {
 
     private static boolean verifyVariableX(double x) {
         if(x < -1 || x > 1) {
-            System.out.println("x не подходит");
+            System.out.println("x не подходит, поскольку не принадлежит интервалу (-1;1)");
             return false;
         }
         return true;
     }
 
-    private static void printResult(double[] result, double function) {
-        System.out.printf("1) Значение функции равно %.5f", result[0]);
-        System.out.println();
-        System.out.printf("2) Значение функции при %.0f слагаемых равно %.5f", result[2], result[1]);
-        System.out.println();
-        System.out.printf("3) Значение функции при %.0f слагаемых равно %.5f", result[4], result[3]);
-        System.out.println();
-        System.out.printf("4) Значение функции 1/(1+x)^2 равно %.5f", function);
+    private static void printResult(double sumOfNSlag, Result resultEpsilon, Result resultEpsilonSumOn10, double function) {
+        System.out.printf("1) Значение функции равно %.5f\n", sumOfNSlag);
+        System.out.printf("2) Значение функции при %d слагаемых равно %.5f\n", resultEpsilon.n, resultEpsilon.sum);
+        System.out.printf("3) Значение функции при %d слагаемых равно %.5f\n", resultEpsilonSumOn10.n, resultEpsilonSumOn10.sum);
+        System.out.printf("4) Значение функции 1/(1+x)^2 равно %.5f\n", function);
     }
 
-    private static double[] calculateSum(double x, int n, double e) {
-        double R = 1;
-        double sum_n = 0, sum_e = 0, sum_e_10 = 0;
-        int n_e = 0, n_e_10 = 0;
-        for(int i = 1; i <= n || Math.abs(R) > e / 10; i++) {
-            if(i <= n) {
-                sum_n += R;
-            }
-            if(Math.abs(sum_e - (sum_e + R)) > e) {
-                sum_e += R;
-                n_e++;
-            }
-            if(Math.abs(sum_e_10 - (sum_e_10 + R)) > e / 10) {
-                sum_e_10 += R;
-                n_e_10++;
-            }
-            R = - R * x * (i + 1) / i;
+    private static double calculateSumOfNSlag(double x, int n) {
+        double slag = 1;
+        double sum = 0;
+        for(int i = 1; i <= n; i++) {
+            sum += slag;
+            slag = - slag * x * (i + 1) / i;
         }
-        return new double[] {sum_n, sum_e, n_e, sum_e_10, n_e_10};
+        return sum;
+    }
+
+    private static Result calculateEpsilonSum(double x, double e) {
+        double slag = 1;
+        double sum = 0;
+        int n = 0;
+        while(Math.abs(sum - (sum + slag)) > e) {
+            sum += slag;
+            n++;
+            slag = - slag * x * (n + 1) / n;
+        }
+        return new Result(n, sum);
+    }
+
+    private static Result calculateEpsilonSumOn10(double x, double e) {
+        double slag = 1;
+        double sum = 0;
+        int n = 0;
+        while(Math.abs(sum - (sum + slag)) > e / 10) {
+            sum += slag;
+            n++;
+            slag = - slag * x * (n + 1) / n;
+        }
+        return new Result(n, sum);
     }
 
     private static double calculateFunction(double x) {
         return 1 / Math.pow(1 + x, 2);
+    }
+
+    static class Result {
+        public int n;
+        public double sum;
+
+        public Result(int n, double sum) {
+            this.n = n;
+            this.sum = sum;
+        }
     }
 }
